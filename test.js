46661e20c46382b462c963924775f53064841c9e
johndove9912 <158753779+johndove9912@users.noreply.github.com>
2024-02-03 21:07:08 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function executeScript() {
     var anchorElements = document.querySelectorAll('a');
     var newurl = "this.com";
     
-    // Loop through each <a> element
+    // Loop through eacha <a> element
     anchorElements.forEach(function(anchorElement) {
         // Check if the text content of the <a> element is "Pay Now"
         if (anchorElement.textContent.trim() === 'Pay Now') {
